429849ec2c4c199de91771d70aa5a4d706508b5d
JaishreeJanu <janujaishree94@gmail.com>
2020-03-13 15:01:58 +0100

    adding inverted_index.py file

--- search_app/views.py
+++ search_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import DocumentForm
+from .inverted_index import Inverted as iv
+import os
 
 # Create your views here.
 def model_form_upload(request):
@@ -11,6 +13,12 @@ def model_form_upload(request):
             return redirect('model_form_upload')
     else:
         form = DocumentForm()
+        path = os.getcwd() + '/documents/'
+        files = os.listdir(path)
+        for filename in files:
+            text = iv.readfile(path, filename)
+            text = iv.preprocess(text) 
+            iv.create_index(text, filename)
     return render(request, 'file_upload_form.html', {
         'form': form
     })
